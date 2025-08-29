dde3df495f5841248a4e26418ca9c21e3503da8b
hariomdeokar15 <hariomdeokar15@gmail.com>
2025-08-30 01:11:05 +0530

    Division.java file added to hariom/division Branch

---
+++ Division.java
@@ -0,0 +1,13 @@
+
+
+class Division{
+  public static void main(String args[]){
+   float a=10f;
+   float b=5f;
+
+   float ans = a/b;
+
+   System.out.println("The answer is : "+ans);
+    
+  }
+}
