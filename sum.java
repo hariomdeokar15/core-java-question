affa2cd54d5748aa19e9319eb557651147f368eb
hariomdeokar15 <hariomdeokar15@gmail.com>
2025-08-30 00:53:16 +0530

    add one functionality to Sum.java

--- sum.java
+++ sum.java
@@ -4,6 +4,9 @@ class Sum{
 public int addition(int a, int b){
 return a+b;
 }
+public int additionOfTwoNumbers(int a, int b, int c){
+  return a+b+c;
+}
 
 public static void main(String args[]){
 
@@ -17,10 +20,15 @@ int a = sc.nextInt();
 System.out.println("Enter a number b");
 int b = sc.nextInt();
 
+System.out.println("Enter a number c");
+int c = sc.nextInt();
+
 
 int answer = s.addition(a, b);
+int answer2 = s.additionOfTwoNumbers(a, b, c);
 
 System.out.println("The Sum of Two numbers are: "+answer);
+System.out.println("The Sum of Three numbers are: "+answer2);
 
 }
 
